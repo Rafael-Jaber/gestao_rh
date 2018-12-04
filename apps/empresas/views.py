9d8811e337d037ddf035308f9378d8f0295e9bb5
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-04 11:07:05 -0200

    CRUD Empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Empresa
 
-# Create your views here.
+
+class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
+
+    def form_valid(self, form):
+        object = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = object
+        funcionario.save()
+        return HttpResponse('Ok')
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
