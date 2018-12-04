0fa4a486cb0d2c16949a0221f9b417fc72a5bec3
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-04 17:31:35 -0200

    Configurado o resto do Create_Documento

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -2,8 +2,15 @@ from django.views.generic.edit import CreateView
 from .models import Documento
 
 
-class DucmentoCreate(CreateView):
+class DocumentoCreate(CreateView):
     model = Documento
     fields = ['descricao', 'arquivo']
 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['funcionario_id']
 
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
