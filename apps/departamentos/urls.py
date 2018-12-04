89c3aa4f3b67a13c5cc712d962b4b78f14855005
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-04 15:48:25 -0200

    Implementado Create_Departamentos

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import DepartamentosList
+from .views import (
+    DepartamentosList,
+    DepartamentoCreate,
+)
 
 urlpatterns = [
     path('', DepartamentosList.as_view(), name='list_departamentos'),
+    path('novo/', DepartamentoCreate.as_view(), name='create_departamento'),
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Departamento
 
 
@@ -9,3 +10,14 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
