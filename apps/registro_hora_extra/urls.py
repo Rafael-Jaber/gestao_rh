1cf530ab5f9a54aaa18bac965243026cc62171b7
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-07 15:10:02 -0200

    Implementado Update Registro-Hora-Extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import HoraExtraList
+from .views import (
+    HoraExtraList,
+    HoraExtraUpdate
+
+)
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name="list_hora-extra"),
-   # path('editar/<int:pk>', FuncionarioEdit.as_view(), name="update_funcionario"),
+    path('editar/<int:pk>', HoraExtraUpdate.as_view(), name="update_hora-extra"),
     #path('deletar/<int:pk>', FuncionarioDelete.as_view(), name="delete_funcionario"),
     #path('novo/', FuncionarioNovo.as_view(), name="create_funcionario"),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, CreateView
 from .models import RegistroHoraExtra
 
 
@@ -9,4 +10,10 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraUpdate(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['horas', 'motivo']
+
 
