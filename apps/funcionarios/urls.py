3c108504d1b02c967d2a22709524b24636081e0e
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-08 22:54:24 -0200

    Implementado o relatorio usando o reportlab

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -6,10 +6,13 @@ from .views import (
     FuncionarioNovo,
 )
 
+from .views import pdf_reportlab
+
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name="list_funcionarios"),
     path('editar/<int:pk>', FuncionarioEdit.as_view(), name="update_funcionario"),
     path('deletar/<int:pk>', FuncionarioDelete.as_view(), name="delete_funcionario"),
     path('novo/', FuncionarioNovo.as_view(), name="create_funcionario"),
+    path('pdf_reportlab', pdf_reportlab, name='gerar_pdf_reportlab')
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -5,6 +5,11 @@ from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Funcionario
 
 
+import io
+from django.http import FileResponse, HttpResponse
+from reportlab.pdfgen import canvas
+
+
 class FuncionariosList(ListView):
     model = Funcionario
 
@@ -35,3 +40,32 @@ class FuncionarioNovo(CreateView):
         funcionario.user = User.objects.create(username=username)
         funcionario.save()
         return super(FuncionarioNovo, self).form_valid(form)
+
+
+def pdf_reportlab(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(200, 810, "Relatório de Funcionarios")
+
+    funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+    str_ = 'Nome: %s | Hora Extra %.2f'
+
+    y = 750
+    for funcionario in funcionarios:
+        p.drawString(10, y, str_ % (funcionario.nome, funcionario.total_horas_extra))
+        y -= 20
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
