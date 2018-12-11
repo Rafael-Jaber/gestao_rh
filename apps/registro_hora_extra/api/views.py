479e9026f861488e3da5fce6e27d6842e588e218
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-11 09:46:40 -0200

    Implementado a segurança da API com o Token

--- apps/registro_hora_extra/api/views.py
+++ apps/registro_hora_extra/api/views.py
@@ -2,8 +2,13 @@ from apps.registro_hora_extra.models import RegistroHoraExtra
 from rest_framework import viewsets
 from .serializers import RegistroHoraExtraSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class RegistroHoraExtraViewSet(viewsets.ModelViewSet):
     queryset = RegistroHoraExtra.objects.all()
     serializer_class = RegistroHoraExtraSerializer
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
