d1c606a16566b13cf87868743c38ac88f06b4a2a
Rafael Jaber <rafael.jaber@gmail.com>
2018-12-11 09:33:33 -0200

    Expondo os dados de hora_extra junto com o funcionário

--- apps/funcionarios/api/serializers.py
+++ apps/funcionarios/api/serializers.py
@@ -1,8 +1,12 @@
 from apps.funcionarios.models import Funcionario
 from rest_framework import serializers
+from apps.registro_hora_extra.api.serializers import RegistroHoraExtraSerializer
 
 
 class FuncionarioSerializer(serializers.ModelSerializer):
+
+    registrohoraextra_set = RegistroHoraExtraSerializer(many=True)
+
     class Meta:
         model = Funcionario
-        fields = ('nome', 'departamentos', 'empresa', 'User')
+        fields = ('id', 'nome', 'departamentos', 'empresa', 'User', 'registrohoraextra_set')
